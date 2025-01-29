a7f2e834d60be4c0035bf9e10deef8de1fe6d8ec
masline2406 <engineer4407@gmail.com>
2025-01-29 11:02:48 -0600

    In class work 1/29

--- flatlistApp/app/index.tsx
+++ flatlistApp/app/index.tsx
@@ -7,8 +7,30 @@ import {
 } from "react-native";
 import colors from "../styles/colors";
 import defaultStyles from "../styles/defaultStyles";
+import { useState } from "react";
 
 export default function Index() {
+
+  type dataType = {
+    id: string,
+    title: string,
+  }
+
+  const DATA: dataType[] = [
+    {id: "1", title: "Jim"},
+    {id: "2", title: "Carla"},
+    {id: "3", title: "Marsha"},
+    {id: "4", title: "Ben"},
+    {id: "5", title: "Gabe"}
+  ]
+
+  const[selectedId, setSelectedId] = useState<string>("")
+
+  const selectedList = (item: dataType) => {
+    alert("Selected " + item.title)
+    setSelectedId(item.id)
+  }
+
   return (
     <View style={defaultStyles.container}>
       <View style={defaultStyles.titleContainer}>
@@ -16,7 +38,15 @@ export default function Index() {
       </View>
       <View style={[defaultStyles.textContainer, { flex: 1 }]}>
         <View style={styles.flatlist}>
-          <Text>This is where our list will go</Text>
+          <FlatList data={DATA} 
+          keyExtractor={(item:dataType) => item.id} 
+          renderItem={({item}) => (
+            <TouchableOpacity onPress={() => selectedList(item)}>
+              <View style={[styles.titleContainer, {backgroundColor: item.id === selectedId ? colors.primary : colors.secondary}]}>
+                <Text style={[styles.titleText, {color: item.id === selectedId ? colors.text.light : colors.text.dark}]}>{item.title}</Text>
+              </View>
+            </TouchableOpacity>
+          )}/>
         </View>
       </View>
     </View>
